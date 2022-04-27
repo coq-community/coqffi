3b3456995569d2f1546d06059331390427a1d3ca
Thomas Letan <lthms@nomadic-labs.com>
2022-04-27 16:06:24 +0200

    Support OCaml 4.13

--- src-prepare.sh
+++ src-prepare.sh
@@ -4,7 +4,8 @@ ocaml_version="$(ocamlc --version|sed - -e 's/\(4\.[0-9][0-9]\)\..*/\1/')"
 coq_version="$(coqc --version|head -n1|sed - -e 's/.*\(8\.[0-9][0-9]\).*/\1/')"
 
 case "${ocaml_version}" in
-    4.11|4.12) true ;;
+    4.13) true ;;
+    4.11|4.12) patch -p1 -u < patches/ocaml.4.11-4.12.patch ;;
     4.10) patch -p1 -u < patches/ocaml.4.10.patch ;;
     4.09|4.08) patch -p1 -u < patches/ocaml.4.08-4.09.patch ;;
     *) echo "unsupported OCaml version ${ocaml_version}"; exit 1 ;;
